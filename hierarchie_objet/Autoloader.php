e92a82d505f5624562343b0483068da7626590c3
Charvet Johnny <Charvet Johnny@outlook.fr>
2022-07-19 12:03:36 +0200

    v2

--- hierarchie_objet/Autoloader.php
+++ hierarchie_objet/Autoloader.php
@@ -4,21 +4,8 @@ class Autoloader{
 
     static function autoload($class){
         $class = str_replace('\\','/',$class); 
-        if(is_file(__DIR__ . '/model/' . $class . '.php')){
-            require __DIR__ . '/model/' . $class . '.php'; 
-        }
-        else{
-            $class = str_replace('\\','/',$class); 
-            if(is_file(__DIR__ . '/model/Animal/' . $class . "/$class.php")){
-                require __DIR__ . '/model/Animal/' . $class . "/$class.php"; 
-            }
-            if(is_file(__DIR__ . '/model/Animal/Herbivore/' . $class . "/$class.php")){
-                require __DIR__ . '/model/Animal/Herbivore/' . $class . "/$class.php"; 
-            }
-            if(is_file(__DIR__ . '/model/Animal/Dog/' . $class . "/$class.php")){
-                require __DIR__ . '/model/Animal/Dog/' . $class . "/$class.php"; 
-            }
-            
+        if(is_file(__DIR__ . '/' . $class . '.php')){
+            require __DIR__ . '/' . $class . '.php'; 
         }
     }
     static function register()
